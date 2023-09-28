459932b59e63e41bf67b0ad85178c1b7c0d50944
Anton <farkind@yandex.ru>
2023-09-28 22:42:50 +0300

    task01 is resolved

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,3 +1,11 @@
 ﻿
-Console.WriteLine("Input theree numbers");
-int numberA = Convert.ToInt32(Console.ReadLine())
+Console.WriteLine("Input three numbers");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberA > numberB ) max = numberA;
+if (numberB > numberA ) max = numberB;
+if (numberC > numberA ) max = numberC;
+Console.Write("max = ");
+Console.WriteLine(max);
